8a0b0d220f7b0ed57329d3b047538efdc21e942c
WinnieCWu <winniechn7@gmail.com>
2022-09-03 17:07:28 -0700

    created signup api route, addressed error handling, and added session object

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -6,15 +6,23 @@ bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    #get all Posts
-    db = get_db()
-    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+  #get all Posts
+  db = get_db()
+  posts = db.query(Post).order_by(Post.created_at.desc()).all()
     
-    return render_template('homepage.html', posts=posts)
+  return render_template(
+    'homepage.html', 
+    posts=posts,
+    loggedIn=session.get('loggedIn')
+  )
 
 @bp.route('/login')
 def login():
-  return render_template('login.html')
+  # not logged in yet
+  if session.get('loggedIn') is None:
+    return render_template('login.html')
+
+  return redirect('/dashboard')
 
 #<id> represents the parameter, captured by single(id)
 @bp.route('/post/<id>')
@@ -26,5 +34,6 @@ def single(id):
   # render single post template
   return render_template(
     'single-post.html',
-    post=post
+    post=post,
+    loggedIn=session.get('loggedIn')
   )
